b4709abd16c39205c50ec455fe081d76f69ea878
BrunoJusta <brunomajusta@gmail.com>
2019-03-15 15:07:04 +0000

    Ex2 done

--- FICHA3/exercicio2.js
+++ FICHA3/exercicio2.js
@@ -1,4 +1,9 @@
 
 //Aliena B
 let b = document.getElementById("p1").innerHTML="Esta um dia de Chuva.";
-console.log(b);
+
+//Alinea C
+let c = document.querySelector("img").src = "https://www.altominho.tv/site/wp-content/uploads/2019/02/chuva.jpg";
+
+//Alinea D
+let d = document.getElementById("p2").style.backgroundColor = "yellow";
