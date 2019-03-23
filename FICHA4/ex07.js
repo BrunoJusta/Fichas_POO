ad3f742143f51603051df5d8f677a44000aadd59
BrunoJusta <brunomajusta@gmail.com>
2019-03-23 16:44:58 +0000

    ex07

--- FICHA4/ex07.js
+++ FICHA4/ex07.js
@@ -1,13 +1,22 @@
 
 
-//document.getElementById("#but").onsubmit = function() {myFunction()};
-
 function myFunction() {
     const table = document.querySelector("table");
-    const titloss= document.querySelector("#tit")
+    const titloss= document.querySelector("#tit");
+    const generos = document.querySelector("#gen");
+    const caps = document.querySelector("#capas");
+    const trail = document.querySelector("#trai");
+
     table.innerHTML +=
     `<tr>
-    <td>${titloss}</td>
+    <td>${titloss.value}</td>
+    <td>${generos.value}</td>
+    <td>
+        <input type="button" onclick= "location.href='${caps.value}'" value="Ver Capa" />
+        <input type="button" onclick= "location.href='${trail.value}'" value="Ver Trailer" />
+        <input type="button" onclick= "${document.querySelector("table").deleteRow(0)}" value="Remover" />
+       
+    </td>
     </tr>
     `
   
