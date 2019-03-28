da1dc64c6a579b62781059ad097e92e76ff08d8b
BrunoJusta <brunomajusta@gmail.com>
2019-03-28 21:10:47 +0000

    fexs

--- ExRevisao/ex.js
+++ ExRevisao/ex.js
@@ -1,20 +1,10 @@
 //funcao que recebe dois numeros e vais escrever na consola todos os numeros multiplos de 5 e que sejam pares.
 
-showNumbers(13,39)
 
-function showNumbers(num1,num2){
-    for(let i=num1; i<=num2; i++){
-
-        if(i%5 === 0 && i%2 === 0){
-            console.log(i)
-        }
-    }
-}
-
-sendNudes()
+sendNudes();
 
 function sendNudes()
 {
-   let   a= prompt("Qual o seu nome?")
-    alert(a+" send nudes ;)")
+   let   a = prompt("Qual o seu nome?")
+    alert(a +" send nudes ;)")
 }
