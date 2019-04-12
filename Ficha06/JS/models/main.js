437624d41d33de4dd2bc29a92f92858d9ca8b71d
BrunoJusta <brunomajusta@gmail.com>
2019-04-12 15:53:12 +0100

    K

--- Ficha06/JS/models/main.js
+++ Ficha06/JS/models/main.js
@@ -3,13 +3,22 @@ import User from "./user.js"
 
 //Array Bandas
 
-export const bands = []
-const band1 = new Band("Muse", "Pop-Rock", "https://semantic-ui.com/images/avatar/large/jenny.jpg", "XXX", "YYY")
-const band2 = new Band("Radiohead", "Pop-Rock", "https://semantic-ui.com/images/avatar/large/jenny.jpg", "XXX", "YYY")
-const band3 = new Band("Metalica", "Metal", "https://semantic-ui.com/images/avatar/large/jenny.jpg", "XXX", "YYY")
-const band4 = new Band("Justa", "Jazz", "https://semantic-ui.com/images/avatar/large/jenny.jpg", "XXX", "YYY")
-const band5 = new Band("James", "Pop-Rock", "https://semantic-ui.com/images/avatar/large/jenny.jpg", "XXX", "YYY")
-bands.push(band1, band2, band3, band4, band5)
+export let bands = []
+if(localStorage.bands){
+    bands = JSON.parse(localStorage.bands)
+}
+else{
+    const band1 = new Band("Muse", "Pop-Rock", "https://semantic-ui.com/images/avatar/large/jenny.jpg", "XXX", "YYY")
+    const band2 = new Band("Radiohead", "Pop-Rock", "https://semantic-ui.com/images/avatar/large/jenny.jpg", "XXX", "YYY")
+    const band3 = new Band("Metalica", "Metal", "https://semantic-ui.com/images/avatar/large/jenny.jpg", "XXX", "YYY")
+    const band4 = new Band("Justa", "Jazz", "https://semantic-ui.com/images/avatar/large/jenny.jpg", "XXX", "YYY")
+    const band5 = new Band("James", "Pop-Rock", "https://semantic-ui.com/images/avatar/large/jenny.jpg", "XXX", "YYY")
+    bands.push(band1, band2, band3, band4, band5)
+    localStorage.setItem("bands",JSON.stringify(bands))
+}
+
+
+
 
 //Array para utilizadores
 export const users = []
